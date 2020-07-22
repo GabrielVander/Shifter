298c028dbcd9436e1c3a30d33a2a6fdf9b6d433b
Gabriel Vander <gabsvander@hotmail.com>
2020-07-21 21:52:56 -0300

    Initial commit

---
+++ app/src/main/java/com/pdm/shifter/MainActivity.java
@@ -0,0 +1,21 @@
+package com.pdm.shifter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pdm.shifter.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
