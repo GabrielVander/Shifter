497fe276ff251b1d2932a7d4137dece1bb3c715e
Gabriel Vander <gabsvander@hotmail.com>
2020-07-28 19:54:02 -0300

    [#1] Added an initial implementation of the main screen

--- app/src/main/java/com/pdm/shifter/MainActivity.java
+++ app/src/main/java/com/pdm/shifter/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
+            getSupportFragmentManager()
+                    .beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
