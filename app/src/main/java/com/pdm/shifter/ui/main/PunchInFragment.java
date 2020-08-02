43f167d45b59d34530c39e6640887ebb2dda042c
Gabriel Vander <gabsvander@hotmail.com>
2020-08-02 19:03:47 -0300

    [#4] Adjusted the "Punch in" button

--- app/src/main/java/com/pdm/shifter/ui/main/PunchInFragment.java
+++ app/src/main/java/com/pdm/shifter/ui/main/PunchInFragment.java
@@ -1,22 +1,18 @@
 package com.pdm.shifter.ui.main;
 
-import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
-import com.pdm.shifter.R;
 import com.pdm.shifter.TimelineAdapter;
 import com.pdm.shifter.databinding.PunchInFragmentBinding;
 import com.pdm.shifter.dummy.DummyContent;
@@ -24,13 +20,13 @@ import com.pdm.shifter.dummy.DummyContent;
 public class PunchInFragment extends Fragment {
 
     private PunchInFragmentBinding binding;
-    private Button mPunchInButton;
     private PunchInViewModel mViewModel;
 
     public static PunchInFragment newInstance() {
         return new PunchInFragment();
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
