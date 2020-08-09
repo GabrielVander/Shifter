c084811b2a4d2893498d372651756d62729c1f26
Gabriel Vander <gabsvander@hotmail.com>
2020-08-09 17:56:44 -0300

    [#8] Implemented Firebase sign up

--- app/src/main/java/com/pdm/shifter/Activity/MainActivity.java
+++ app/src/main/java/com/pdm/shifter/Activity/MainActivity.java
@@ -1,29 +1,79 @@
 package com.pdm.shifter.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.pdm.shifter.Fragment.MainFragment;
 import com.pdm.shifter.Fragment.TodaysPunchesFragment;
 import com.pdm.shifter.R;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityResultLauncher<Intent> mLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    IdpResponse response = IdpResponse.fromResultIntent(result.getData());
+
+                    if (result.getResultCode() == RESULT_OK) {
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        assert user != null;
+                        Toast.makeText(MainActivity.this, getString(R.string.signed_in_as, user.getDisplayName()), Toast.LENGTH_SHORT).show();
+                        setupView();
+                    } else {
+                        Toast.makeText(MainActivity.this, R.string.sign_in_failed, Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
+            if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                List<AuthUI.IdpConfig> providers = Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.GoogleBuilder().build()
+                );
 
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.bottom_container, TodaysPunchesFragment.newInstance())
-                    .commitNow();
+                mLauncher.launch(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .setTheme(R.style.AppTheme)
+                                .build()
+                );
+            } else {
+                setupView();
+            }
         }
+    }
+
+    private void setupView() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.bottom_container, TodaysPunchesFragment.newInstance())
+                .commitNow();
     }
 }
